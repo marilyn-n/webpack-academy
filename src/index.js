8bf6a56658cad2e621c87e54647bd3a98640c4f7
Ravi Kalia <ravkalia@gmail.com>
2018-11-27 10:07:28 -0600

    refactor(index.js): clean up formatting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-console.log('Hello from webpack academy!!!!');
-
 import fooString from './foo.js';
 import bar from './bar-cjs'; 
 
+console.log('Hello from webpack academy!!!!');
 console.log(fooString, bar);
