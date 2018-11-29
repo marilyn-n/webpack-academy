2b79ddfdb80ab4d220b8923cf4cffd5a8d269a5a
Ravi Kalia <ravkalia@gmail.com>
2018-11-29 17:23:35 -0600

    feat(url-loader): inline base64 url contents into bundle, up to option size
    reduces number of http requests

--- build_utils/webpack.common.js
+++ build_utils/webpack.common.js
@@ -9,6 +9,19 @@ const config = {
     filename: '[chunkhash]bundle.js',
     path: commonPaths.outputPath,
   },
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+          },
+        },
+        ],
+      }],
+  },
   plugins: [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin(),
