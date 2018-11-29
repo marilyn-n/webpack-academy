b2f5f6fb71abb1b30aafa50b2c0e982ad4c65750
Ravi Kalia <ravkalia@gmail.com>
2018-11-29 15:41:35 -0600

    feat(progressPlugin): add progress plugin from webpack

--- build_utils/webpack.common.js
+++ build_utils/webpack.common.js
@@ -1,5 +1,5 @@
 const commonPaths = require('./common-paths');
-
+const webpack = require('webpack');
 const config = {
   entry: './src/index.js',
   mode: 'development',
@@ -7,6 +7,9 @@ const config = {
     filename: 'bundle.js',
     path: commonPaths.outputPath,
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+  ],
 };
 
 module.exports = config;
