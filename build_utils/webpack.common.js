a13eed51e8610840cf34ce733a353d612499dd0f
Ravi Kalia <ravkalia@gmail.com>
2018-11-29 15:45:27 -0600

    feat(html-webpack-plugin): add html plugin to create index file with bundles, allows hashing

--- build_utils/webpack.common.js
+++ build_utils/webpack.common.js
@@ -1,14 +1,17 @@
 const commonPaths = require('./common-paths');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: '[chunkhash]bundle.js',
     path: commonPaths.outputPath,
   },
   plugins: [
     new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin(),
   ],
 };
 
