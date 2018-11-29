2a52105a1c6750ed41bb9a6e3b5f4c2cbfd25dcf
Ravi Kalia <ravkalia@gmail.com>
2018-11-29 13:50:46 -0600

    feat(webpack.bundle.analyzer.js): install and run bundle analyzer tool

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const commonConfig = require('./build_utils/webpack.common');
 const webpackMerge = require('webpack-merge');
 
-module.exports = (env) => {
-  console.log(env);
-  const envConfig = require(`./build_utils/webpack.${env.env}.js`); 
-  return webpackMerge(commonConfig, envConfig);
+const addons = (addonsArg) => {
+  return ([]
+    .concat
+    .apply([], [addonsArg])
+    .filter(Boolean)
+    .map(addonName => require(`./build_utils/addons/webpack.${addonName}.js`)));
 };
 
+module.exports = (env) => {
+  console.log(env);
+  const envConfig = require(`./build_utils/webpack.${env.env}.js`);
+  return webpackMerge(commonConfig, envConfig, ...addons(env.addons));
+};
