06d021433eeac9b1cc5d225e5c8b27b65b884cf8
Ravi Kalia <ravkalia@gmail.com>
2018-11-30 12:33:33 -0600

    feat(extract-text-webpack-plugin): add text extraction for css and style loaders

--- build_utils/webpack.prod.js
+++ build_utils/webpack.prod.js
@@ -1,5 +1,21 @@
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
 const config = {
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: 'css-loader',
+          fallback: 'style-loader',
+        }),
+  },
+],
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css'),
+  ],
+
 };
 
 module.exports = config;
