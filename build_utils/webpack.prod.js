756f5da443fd2cc7bb9d172d1aa970bf417b0a5c
Ravi Kalia <ravkalia@gmail.com>
2018-11-30 12:49:12 -0600

    feat(uglify): add uglify plugin to reduce size of bundle

--- build_utils/webpack.prod.js
+++ build_utils/webpack.prod.js
@@ -1,19 +1,22 @@
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   devtool: 'source-map',
   module: {
-    rules: [
-      {
-        test: /\.png$/,
-        use: ExtractTextWebpackPlugin.extract({
-          use: 'css-loader',
-          fallback: 'style-loader',
-        }),
+    rules: [{
+      test: /\.png$/,
+      use: ExtractTextWebpackPlugin.extract({
+        use: 'css-loader',
+        fallback: 'style-loader',
+      }),
+    }],
   },
-],
   plugins: [
     new ExtractTextWebpackPlugin('styles.css'),
+    new UglifyJsWebpackPlugin({
+      sourcemap: true,
+    }),
   ],
 
 };
